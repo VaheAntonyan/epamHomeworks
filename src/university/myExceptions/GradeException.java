bc5610afcb4f18c37e9eb0e916e15f239e6f21af
Vahe Antonyan <vaheant@gmail.com>
2020-01-17 13:32:00 +0400

    Fixed GradeException: Now it does look like a custom exception, not a generic one.

--- src/university/myExceptions/GradeException.java
+++ src/university/myExceptions/GradeException.java
@@ -1,7 +1,10 @@
 package university.myExceptions;
 
+import university.Constants;
+
 public class GradeException extends Exception {
-    public GradeException(String message) {
-        super(message);
+    private static String ERROR_MSG = "Invalid Grade: Grade must be between [" + Constants.MIN_GRADE + ", " + Constants.MAX_GRADE + "]: Provided grade: %s";
+    public GradeException(int grade) {
+        super(String.format(ERROR_MSG, grade));
     }
 }
